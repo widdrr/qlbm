565f4ad0e63f35370711833b93369168a198dde5
widdrr <tavaandrei@gmail.com>
2026-03-28 10:52:18 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "QLBM — Quantum Lattice Boltzmann Method";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-medium
+            qcircuit
+            latexmk;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            uv
+            tex
+            poppler-utils
+          ];
+
+          shellHook = "";
+        };
+      }
+    );
+}
